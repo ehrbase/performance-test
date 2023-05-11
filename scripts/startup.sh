f2326c7dd02caef639566a72de04d31008f7a7c6
vmueller-vg <100680528+vmueller-vg@users.noreply.github.com>
2023-05-11 18:04:10 +0200

    Update startup.sh

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -2,9 +2,9 @@
 set -e
 
 helm install ehrbase-load-test-deployment ~/ehrbase-helm-chart-hetzner/
-
-until [ $(curl -s -o /dev/null -w "%{http_code}" "http://localhost:30901/ehrbase/rest/status") == 200 ] &&
-  [ $(curl -s -u webtester:Dctm1234 "http://localhost:30902/webtester/actuator/health" | jq '.status') == "\"UP\"" ]; do
+KUBE_IP="$(minikube ip)"
+until [ $(curl -s -o /dev/null -w "%{http_code}" "http://$KUBE_IP:30901/ehrbase/rest/status") == 200 ] &&
+  [ $(curl -s -u webtester:Dctm1234 "http://$KUBE_IP:30902/webtester/actuator/health" | jq '.status') == "\"UP\"" ]; do
   echo "Installing EHRbase and WebTester..."
   sleep 30
 done
